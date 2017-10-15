d36a0a19699010b9bae521ad67ee507c636525dc
Luan Nico <luanpotter27@gmail.com>
2017-10-15 16:16:12 -0200

    deploying cora

--- projects/pull_cora.sh
+++ projects/pull_cora.sh
@@ -5,7 +5,7 @@ mkdir cora
 
 cd ../../cora
 yarn
-webpack
+./node_modules/.bin/webpack
 cp index.html ../luanpotter.github.io/projects/cora/
 cp bundle.js ../luanpotter.github.io/projects/cora/
 cp -r images ../luanpotter.github.io/projects/cora/
