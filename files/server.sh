746fe773f3001b94c9d10430a879a7e3dcdb3042
Luan Nico <luanpotter27@gmail.com>
2018-03-29 12:33:25 -0300

    adding simplest of servers

---
+++ files/server.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
+while { echo -en "$RESPONSE"; } | nc -l "${1:-8080}"; do
+    mpg321 jobs_done.mp3
+    echo "Played"
+done
