c66b293a06954d9d875b9cf561aa86cfddc99d6b
Luan Nico <luanpotter27@gmail.com>
2020-04-19 20:56:06 -0400

    Update life2

--- exp/life2/flutter_service_worker.js
+++ exp/life2/flutter_service_worker.js
@@ -1,14 +1,15 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
-"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+  "assets/assets/fonts/Chonkly.otf": "d798ff9eabad6d7c4bc7e8b0fa8738d1",
+"assets/FontManifest.json": "08ce1b3b2184a269062e7d64c07de9e7",
+"assets/AssetManifest.json": "1b89573fa5a30281e1273e4ebeedbab9",
 "assets/LICENSE": "65a84aefcc0d59ed02da4f4dc733070a",
-"manifest.json": "b5a73da24426a12011ab1373ccb037f2",
+"manifest.json": "9af27539b7a61934c4c730b2d65140ed",
 "index.html": "e2c4015457c201e564a93e3a6fb6a474",
 "/": "e2c4015457c201e564a93e3a6fb6a474",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "c4597747f457d052e686b80191e0d8d8",
+"main.dart.js": "2b19042a5ff4d347d10bbcd9a5fdcb0e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };
