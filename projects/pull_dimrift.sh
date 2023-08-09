9e4fbb00432197680755cb578fc6a4f9792f1f1f
Luan Nico <luanpotter27@gmail.com>
2023-08-08 23:41:04 -0400

    Update dim-rift

--- projects/pull_dimrift.sh
+++ projects/pull_dimrift.sh
@@ -3,7 +3,7 @@
 rm -rf dim-rift
 
 cd ../../gamedev/dimensional-rift
-./scripts/build-site.sh
+ENV=production ./scripts/build-site.sh
 
 cd ../../luanpotter.github.io/projects
 cp -r ../../gamedev/dimensional-rift/site/_site/ dim-rift
