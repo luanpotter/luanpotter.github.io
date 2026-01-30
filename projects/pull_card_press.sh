832c814223e69a5c92fe36fb20cfb4c6b0d6ede6
Luan Nico <luanpotter27@gmail.com>
2026-01-29 22:07:45 -0500

    Deploy card-press

--- projects/pull_card_press.sh
+++ projects/pull_card_press.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -euo pipefail
+
 rm -rf card-press
 mkdir card-press
 
 cd ../../card-press
 ./scripts/build.sh
-sed -i "s/\[\[\[VERSION\]\]\]/$(git rev-parse --short HEAD)/g" dist/assets/*.js
+sed -i "s/\[\[\[VERSION\]\]\]/$(git rev-parse --short HEAD)/g" dist/*.js
 cd -
 
 cp -r ../../card-press/dist/* ./card-press
