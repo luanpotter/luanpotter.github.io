bea53dd7250093b9338f3249d1839e97ad53388e
Luan Nico <luanpotter27@gmail.com>
2026-02-06 22:26:18 -0500

    Generify blog posts and articles as "content"

--- src/utils/collections.ts
+++ src/utils/collections.ts
@@ -1,9 +1,16 @@
 import type { CollectionEntry } from "astro:content";
 import { getCollection } from "astro:content";
 
+export type Article = CollectionEntry<"articles"> & {
+  path: string;
+};
+
 export type BlogPost = CollectionEntry<"blog"> & {
+  path: string;
   data: CollectionEntry<"blog">["data"] & { date: Date };
 };
+
+export type ContentEntry = Article | BlogPost;
 
 function parseDateFromFilename(id: string): Date {
   const match = id.match(/^(\d{4})-(\d{2})-(\d{2})-/);
@@ -20,15 +27,30 @@ export async function getBlogPosts(): Promise<BlogPost[]> {
     .filter((post) => !post.id.startsWith("_"))
     .map((post) => ({
       ...post,
+      path: `/blog/${post.id}`,
       data: {
         ...post.data,
         date: parseDateFromFilename(post.id),
       },
     }))
-    .sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
+    .sort(sortByDate((post) => post.data.date));
 }
 
-export async function getArticles(): Promise<CollectionEntry<"articles">[]> {
+export async function getArticles(): Promise<Article[]> {
   const articles = await getCollection("articles");
-  return articles.sort((a, b) => b.data.date.getTime() - a.data.date.getTime());
+  return articles
+    .map((article) => ({
+      ...article,
+      path: `/articles/${article.id}`,
+    }))
+    .sort(sortByDate((item) => item.data.date));
+}
+
+export async function getContent(): Promise<ContentEntry[]> {
+  const [articles, blogPosts] = await Promise.all([getArticles(), getBlogPosts()]);
+  return [...articles, ...blogPosts].sort(sortByDate((item) => item.data.date));
+}
+
+export function sortByDate<T>(getDate: (item: T) => Date) {
+  return (a: T, b: T) => getDate(b).getTime() - getDate(a).getTime();
 }
