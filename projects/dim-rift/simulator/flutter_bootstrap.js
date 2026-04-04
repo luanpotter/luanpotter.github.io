9230c1be8cf09906ebdf172a133a3004edfb8a57
Luan Nico <luanpotter27@gmail.com>
2026-04-03 21:54:54 -0400

    Deploy dim-rift

--- projects/dim-rift/simulator/flutter_bootstrap.js
+++ projects/dim-rift/simulator/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"1306bcd2f6219f4dda92deed042c6f838f35ff
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1200530756" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2696816313" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
